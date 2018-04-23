ca6f284f252a38dec262aa2b57e1d95bfbbeef7b
frostwilly <frosthaki@gmail.com>
2018-04-23 20:34:16 +0700

    Sushi~

--- promoMenu.php
+++ promoMenu.php
@@ -4,7 +4,7 @@
     while($data = $promoIterator->getNextMenu()) {
         ?>
         <div class="menubox shadowbox">
-            <img class="foodpicture" src="images/Pika.png">
+            <img class="foodpicture" src="images/Sushi/Promo/<?= str_replace(' ','',$data->getName()); ?>.jpg">
             <div class="foodname"><a><?php echo $data->getName(); ?></a></div>
             <span class="fooddetail"><p>Price : <?php echo $data->getPrice(); ?></p></span>
 			<input class="foodid" type="hidden" value="<?php echo $data->getId(); ?>">
